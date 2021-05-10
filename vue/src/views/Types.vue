1e2a516c07a9b0ce1fddc819e3c1188739f3c21a
yoongbok lee <yoongbok.lee@certik.org>
2021-05-10 08:51:12 -0400

    remove create orchestrator addr from frontend

--- vue/src/views/Types.vue
+++ vue/src/views/Types.vue
@@ -2,7 +2,6 @@
 	<div>
 		<div class="sp-container">
       	<!-- this line is used by starport scaffolding # 4 -->
-		<SpType modulePath="gravity.v1" moduleType="OrchestratorAddress"  />
 		<SpType modulePath="gravity.v1" moduleType="CosmosToEth"  />
 		</div>
 	</div>
